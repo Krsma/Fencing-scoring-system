016e8de68254cca10580193a3d9bc0a575a67614
Krsma <vlada.krsmanovic@gmail.com>
2018-07-31 16:35:14 +0200

    added callback handlers and hit detection

--- central-score-machine/central-score-machine.ino
+++ central-score-machine/central-score-machine.ino
@@ -5,13 +5,10 @@
 #include "BLEDevice.h"
 //#include "BLEScan.h"
 
-// The remote service we wish to connect to.
 static BLEUUID serviceUUID1("91bad492-b950-4226-aa2b-4ede9fa42f59");  //TODO check perif and central device UUIDs
-// The characteristic of the remote service we are interested in.
 static BLEUUID    charUUID1("0d563a58-196a-48ce-ace2-dfec78acc814");
 
 static BLEUUID serviceUUID2("dbf07658-78d4-40af-be4b-ef230e0def62");
-// The characteristic of the remote service we are interested in.
 static BLEUUID    charUUID2("afa9c82b-1443-409d-bb42-1c51adc6abc7");
 
 static BLEAddress *pServerAddress1;
@@ -26,16 +23,59 @@ static boolean connected = false;
 static BLERemoteCharacteristic* pRemoteCharacteristic1;
 static BLERemoteCharacteristic* pRemoteCharacteristic2;
 
+int LocalTimer1 = 0;
+int LocalTimer2 = 0;
+int TimeDiff = 0;
 
-static void notifyCallback( //TODO implement system fir reading data - didnt touch notify yet
-  BLERemoteCharacteristic* pBLERemoteCharacteristic,
-  uint8_t* pData,
-  size_t length,
-  bool isNotify) {
-    Serial.print("Notify callback for characteristic ");
-    Serial.print(pBLERemoteCharacteristic->getUUID().toString().c_str());
-    Serial.print(" of data length ");
-    Serial.println(length);
+int LastSynchedTimestamp = 0;
+
+int touch1 = 0;
+int touch2 = 0;
+bool Hit1 = false;
+bool Hit2 = false;
+
+bool AlreadyNotified = false;
+
+
+union
+{
+int Timer_;
+uint8_t union_data[4];
+}converter_union;
+
+//TODO implement system fir reading data - didnt touch notify yet
+static void notifyCallback( BLERemoteCharacteristic* pBLERemoteCharacteristic,uint8_t* pData,size_t length,bool isNotify)
+{
+  if (AlreadyNotified == false)
+  {
+    AlreadyNotified = true;
+    int touch1 = 0;
+    int touch2 = 0;
+    // Serial.print("Notify callback for characteristic ");
+    //Serial.print(pBLERemoteCharacteristic->getUUID().toString().c_str());
+    //Serial.print(" of data length ");
+    // Serial.println(length);
+    ReadTimerData();
+    touch1 = LocalTimer1;
+    touch2 = LocalTimer2 + TimeDiff; // leveling two timer , taking in account the time diff
+    //if LocalTimer1 is bigger the LT2 then the addition increases the LT2, if its the other way around and 
+    //the negative value of TimeDiff takes off the advantadge of LT2
+    delay(40);
+    ReadTimerData();  //reading timer again after the 40ms interval to check for hits; after the hit the timer gets locked and we check that
+    if (touch1 + 40 > LocalTimer1)
+    {
+      Hit1 = true;
+    }
+    if (touch2 + 40 > LocalTimer2)
+    {
+      Hit2 = true;
+    }
+    delay(5000);
+
+  }
+    
+
+
 }
 
 bool connectToServer(BLEAddress pAddress1, BLEAddress pAddress2) {
@@ -168,15 +208,24 @@ void loop() {
     if (connectToServer(*pServerAddress1,*pServerAddress2)) {
       Serial.println("We are now connected to the BLE Servers.");
       connected = true;
+      delay(2000);
+      GetLocalTimers();
+      LastSynchedTimestamp = millis();
+      //TODO Visual indication all is ok
     } else {
       Serial.println("We have failed to connect to the servers; there is nothin more we will do.");
     }
     doConnect = false;
   }
 
-  // If we are connected to a peer BLE Server, update the characteristic each time we are reached
-  // with the current time since boot.
+
   if (connected) {
+      int Timenow = millis();
+      if (LastSynchedTimestamp + 15000 < Timenow) //synching due to millis() drift
+      {
+        GetLocalTimers();
+        LastSynchedTimestamp = Timenow;
+      }
   //  String newValue = "Time since boot: " + String(millis()/1000);
    // Serial.println("Setting new characteristic value to \"" + newValue + "\"");
     
@@ -186,3 +235,32 @@ void loop() {
   
   delay(1000); // Delay a second between loops.
 } // End of loop
+
+void GetLocalTimers()
+{
+  ReadTimerData();
+
+  TimeDiff = LocalTimer1 - LocalTimer2;
+  //LocalTimer1
+  //LocalTimer2
+  //time diff = timer1 - timer2
+}
+void ReadTimerData()
+{
+  std::string TValue1 = pRemoteCharacteristic1->readValue();    //more work needed fo conversion from string to int
+  std::string TValue2 = pRemoteCharacteristic2->readValue();
+
+
+  converter_union.union_data[0] = TValue1[0];
+  converter_union.union_data[1] = TValue1[1];
+  converter_union.union_data[2] = TValue1[2];
+  converter_union.union_data[3] = TValue1[3];
+  LocalTimer1 = converter_union.Timer_;
+
+  converter_union.union_data[0] = TValue2[0];
+  converter_union.union_data[1] = TValue2[1];
+  converter_union.union_data[2] = TValue2[2];
+  converter_union.union_data[3] = TValue2[3];
+  LocalTimer2 = converter_union.Timer_;
+  
+}
